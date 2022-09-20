7f2a67a59d3068ef6f7055434335811d4486c97a
Java Zone <javazoneandbistro@gmail.com>
2022-09-20 16:37:12 +0545

    components added

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function clickAlert() {
+    alert("This site is under construction.");
+}
+function clickCat1(){
+    alert("You clicked category 1.");
+}
+function clickCat2(){
+    alert("You clicked category 2.");
+}
+function clickCat3(){
+    alert("You clicked category 3.");
+}
+function clickCat4(){
+    alert("You clicked category 4.");
+}
+function clickCat5(){
+    alert("You clicked category 5.");
+}
